77c1dd2224723e109f742b65992812cc6a4ac685
antonioHenrique-Git <antonio.henrique.silvaa@gmail.com>
2026-09-16 15:16:04 -0300

    Alterando presso

--- allbooks-aula-01/src/routes/publicRoutes.js
+++ allbooks-aula-01/src/routes/publicRoutes.js
@@ -298,7 +298,7 @@ server.get('/mais-vendidos', (req, res) => {
                 {
                     "id": 3,
                     "titulo": "E-book + Impresso",
-                    "preço": 59.9,
+                    "presso": 59.9,
                     "formatos": [
                         ".pdf",
                         ".pub",
